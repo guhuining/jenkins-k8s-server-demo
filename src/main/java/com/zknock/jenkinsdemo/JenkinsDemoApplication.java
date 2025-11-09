5ab024908b4269a8077ef216f74669497b676fbd
huining <305579164@qq.com>
2025-11-09 20:06:18 +0800

    init

---
+++ src/main/java/com/zknock/jenkinsdemo/JenkinsDemoApplication.java
@@ -0,0 +1,23 @@
+package com.zknock.jenkinsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/api/v1")
+public class JenkinsDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsDemoApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String download() throws IOException {
+        return "hello";
+    }
+}
